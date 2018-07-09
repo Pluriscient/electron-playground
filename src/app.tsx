ce04192ff33fd9a216c92d6cc572415b3ff356ef
Eric van der Toorn <erictoorn@gmail.com>
2018-07-10 01:13:36 +0200

    initial

---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export class App extends React.Component<undefined, undefined> {
+  render() {
+    return (
+      <div>
+        <h2>Welcome to React with Typescript 2.0!</h2>
+      </div>
+    );
+  }
+}
